32a0f11c94aca5df488f4943b5f67ebcd30f0944
JevinWilson <jaw062701@gmail.com>
2024-01-28 18:10:28 -0500

    updated new roulette, still don't know how to run it in the browser

--- Roulette/html/roulette.js
+++ Roulette/html/roulette.js
@@ -1,89 +1,60 @@
-function Roulette() {
-    // roulette numbers
-    let rouletteOrder = [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, "00", 15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36];
+"use strict";
 
-    // store roulette outcomes
-    let rouletteOutcome = {};
 
-    // french names
-    let frenchNames = {
-        0: "zero, ",
-        1: "un, ",
-        2: "deux, ",
-        3: "trois, ",
-        4: "quatre, ",
-        5: "cinq, ",
-        6: "six, ",
-        7: "sept, ",
-        8: "huit, ",
-        9: "neuf, ",
-        10: "dix, ",
-        11: "onze, ",
-        12: "douze, ",
-        13: "treize, ",
-        14: "quatorze, ",
-        15: "quinze, ",
-        16: "seize, ",
-        17: "dix-sep, t",
-        18: "dix-huit, ",
-        "00": "zero, ",
-        19: "dix-neuf, ",
-        20: "vingt, ",
-        21: "vingt et un, ",
-        22: "vingt-deux, ",
-        23: "vingt-trois, ",
-        24: "vingt-quatre, ",
-        25: "vingt-cinq, ",
-        26: "vingt-six, ",
-        27: "vingt-sept, ",
-        28: "vingt-huit, ",
-        29: "vingt-neuf, ",
-        30: "trente, ",
-        31: "trente et un, ",
-        32: "trente-deux, ",
-        33: "trente-trois, ",
-        34: "trente-quatre, ",
-        35: "trente-cinq, ",
-        36: "trente-six, "
-    }
+// roulette outcome [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, 00, 15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36]
+let rouletteOutcome = [
+    {number: 0, french: "zero", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 34, french: "trois-quatre", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: 10, french: "dix", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 21, french: "vingt-et-un", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 28, french: "vingt-huit", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: 4, french: "quatre", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 18, french: "dix-huit", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 9, french: "neuf", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", manque"},
+    {number: 27, french: "vingt-sept", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 22, french: "vingt-deux", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: 12, french: "douze", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 3, french: "trois", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 17, french: "sept", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 20, french: "vingt", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 11, french: "onze", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 33, french: "treize", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 2, french: "deux", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 10, french: "dix", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 32, french: "treize", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: "00", french: "zero", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: 15, french: "quinze", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", manque"},
+    {number: 8, french: "huit", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 25, french: "vingt-cinq", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 1, french: "un", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", manque"},
+    {number: 31, french: "treize", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 20, french: "vingt", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: 14, french: "quatorze", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 30, french: "treize", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: 7, french: "sept", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", manque"},
+    {number: 24, french: "vingt-quatre", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", passe"},
+    {number: 29, french: "vingt-neuf", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 35, french: "trente-cinq", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 6, french: "six", color: ", rouge", oddOrEven: ", pair", manqueOrPasse: ", manque"},
+    {number: 13, french: "treize", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", manque"},
+    {number: 23, french: "vingt-trois", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 19, french: "neuf", color: ", noir", oddOrEven: ", impair", manqueOrPasse: ", passe"},
+    {number: 5, french: "cinq", color: ", rouge", oddOrEven: ", impair", manqueOrPasse: ", manque"},
+    {number: 36, french: "trente-six", color: ", noir", oddOrEven: ", pair", manqueOrPasse: ", passe"}
+]
 
-    // store roulette outcome
-    rouletteOrder.forEach((number, index) => {
-        let color;
-        let evenOrOdd;
-        let manqueOrPasse;
+function roulette() {
+    // get random index: https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/
+    let randomIndex = Math.floor(Math.random() * rouletteOutcome.length);
+    let winner = rouletteOutcome[randomIndex];
 
-        if (number === 0) {
-            color = ", rouge";
-        }
-        else if (number === "00") {
-            color = ", noir";
-        }
-        else {
-            // alternate color down the list
-            color = (index % 2 === 0)? ", rouge" : ", noir";
-        }
+    // output result
+    console.log('${winner.number} ${winner.french} ${winner.color} ${winner.oddOrEven} ${winner.manqueOrPasse}');
 
-        if (number != 0 && number!= "00") {
-            evenOrOdd = (number % 2 === 0)? ", pair" : ", impair";
-        }
+    // output to display: https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+    let display = document.getElementById('display');
+    display.textContent = `${winner.number} ${winner.french} ${winner.color} ${winner.oddOrEven} ${winner.manqueOrPasse}`;
 
-        if (number != "00" && number <= 18) {
-            manqueOrPasse = "manque";
-        } 
-        else if (number != "00" && number > 18) {
-            manqueOrPasse = "passe";
-        }
-
-        rouletteOutcome[number] = {
-            frenchNames: frenchNames[number],
-            color: color,
-            evenOrOdd: evenOrOdd,
-            manqueOrPasse: manqueOrPasse
-        };
-    });
-
-    console.log(rouletteOutcome);
+    // call program on start up
+    window.onload = roulette();
 }
-
-Roulette();
